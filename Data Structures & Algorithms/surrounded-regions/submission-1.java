972ceb2a16a145738a592a3ceaeef2e80c9e654c
Hemant Kumar <64378827+hemantkumar00@users.noreply.github.com>
2026-05-28 08:57:20 +0530

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void solve(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+
+        for (int i = 0; i < n; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, m - 1);
+        }
+
+        for (int j = 0; j < m; j++) {
+            dfs(board, 0, j);
+            dfs(board, n - 1, j);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        int n = board.length;
+        int m = board[0].length;
+
+        if (i < 0 || i >= n || j < 0 || j >= m || board[i][j] != 'O') {
+            return;
+        }
+
+        board[i][j] = '#';
+
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+}
